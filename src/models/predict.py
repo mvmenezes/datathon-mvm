0d43456a07b8bb809faff2552dd4aa35fef3b891
CURITIBA\mmenezes <mmenezes@alta-rt.com>
2026-04-29 04:29:07 -0300

    teste workflow

--- src/models/predict.py
+++ src/models/predict.py
@@ -29,7 +29,7 @@ def predict(params: PredictParams):
         model.eval()
         with torch.no_grad():
             pred = model(X_torch).squeeze()
-    pred_np = pred.item().numpy().reshape(-1,1)
+    pred_np = pred.detach().cpu().numpy().reshape(-1, 1)
     dummy = np.zeros((len(pred_np),6))
     dummy[:,0] = pred_np[:,0]
     pred_real = scaler.inverse_transform(dummy)[:,0]
